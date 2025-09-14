842a7d3e85370be0b51a0051b5c53775d99a28bc
AnilKumt <anilkumt51@gmail.com>
2025-09-14 05:52:47 +0530

    setup home page video and downloaded fonts

---
+++ src/components/home/HomeHeroText.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HomeHeroText = () => {
+  return (
+    <div>
+      <div>
+        <div>THE SPARK FOR</div>
+        <div>ALLTHINGS</div>
+        <div>CREATIVE</div>
+      </div>
+    </div>
+  )
+}
+
+export default HomeHeroText
