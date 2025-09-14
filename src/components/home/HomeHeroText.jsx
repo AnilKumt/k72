b33aad25643f67dddc86d217a2ed6f301bf06fbf
AnilKumt <anilkumt51@gmail.com>
2025-09-14 07:47:44 +0530

    made top of the home page headings

--- src/components/home/HomeHeroText.jsx
+++ src/components/home/HomeHeroText.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
+import React from "react";
+import Video from "./Video";
 
 const HomeHeroText = () => {
   return (
     <div>
-      <div>
-        <div>THE SPARK FOR</div>
-        <div>ALLTHINGS</div>
-        <div>CREATIVE</div>
+      <div className="font-[font1] pt-5 text-center">
+        <div className="text-8xl uppercase leading-[8vw] justify-center flex items-center">The Spark for</div>
+        <div className="text-8xl uppercase leading-[8vw] flex items-center justify-center">
+          all
+          <div className="h-[6vw] w-[12vw] rounded-full overflow-hidden">
+            <Video/>
+          </div>
+          things
+        </div>
+        <div className="text-8xl uppercase leading-[8vw] justify-center flex items-center">creative</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomeHeroText
+export default HomeHeroText;
--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 
-const Video = () => {
-  return (
-    <div>
-        <video className='h-full w-full object-cover' autoPlay loop muted src='https://download-video-ak.vimeocdn.com/v3-1/playback/a278bb9c-996c-4760-8ca1-dd0abff308f7/83c745cf?__token__=st=1757807602~exp=1757811202~acl=%2Fv3-1%2Fplayback%2Fa278bb9c-996c-4760-8ca1-dd0abff308f7%2F83c745cf%2A~hmac=7403f0db19dc0547e285578262a2b5cdb77e469d68b812b6f03408e4e46d48b2&r=dXMtd2VzdDE%3D'>
-        </video>
-    </div>
-  )
-}
+const Video = ({ className = '' }) => (
+  <div className={`h-full w-full ${className}`}>
+    <video className="h-full w-full object-cover rounded-3xl" autoPlay loop muted src="video/intro.mp4" />
+  </div>
+)
+
 
 export default Video
