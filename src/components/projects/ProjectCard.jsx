fa15f1d97a0528d1f0fd79a2e4d64ca41b5635ef
AnilKumt <anilkumt51@gmail.com>
2025-09-17 23:03:34 +0530

    completed projects page

---
+++ src/components/projects/ProjectCard.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const ProjectCard = ({image1,image2}) => {
+  return (
+    <>
+          <div className="w-1/2 group h-full relative overflow-hidden">
+            <img
+              className="h-full w-full rounded-none group-hover:rounded-[50px] transition-all object-cover"
+              src={image1}
+            />
+            <div className="opacity-0 cursor-pointer transition-all group-hover:opacity-100 absolute top-0 left-0 h-full w-full bg-black/10 flex justify-center items-center">
+              <h2 className="uppercase text-white border-white text-6xl font-[font1] border-2 pt-4 px-4 rounded-full ">
+                view Project
+              </h2>
+            </div>
+          </div>
+          <div className="w-1/2 group h-full relative overflow-hidden">
+            <img
+              className="h-full w-full rounded-none group-hover:rounded-[50px] transition-all object-cover"
+              src={image2}
+            />
+            <div className="opacity-0 cursor-pointer transition-all group-hover:opacity-100 absolute top-0 left-0 h-full w-full bg-black/10 flex justify-center items-center">
+              <h2 className="uppercase text-white border-white text-6xl font-[font1] border-2 pt-4 px-4 rounded-full ">
+                view Project
+              </h2>
+            </div>
+          </div>
+    </>
+  )
+}
+
+export default ProjectCard
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import React from "react";
+import ProjectCard from "../components/projects/ProjectCard";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+const projectImages = [
+  {
+    image1:'https://k72.ca/uploads/caseStudies/COUP_FUMANT/CF_thumbnail-1280x960.jpg',
+    image2:'https://k72.ca/uploads/caseStudies/SHELTON/thumbnailimage_shelton-1280x960.jpg'
+  },
+  {
+    image1:'https://k72.ca/uploads/caseStudies/A_table/thumbnailimage_atable2-1280x960.jpg',
+    image2:'https://k72.ca/uploads/caseStudies/SollioAg/thumbnailimage_SollioAg-1280x960.jpg'
+  },
+  {
+    image1:'https://k72.ca/uploads/caseStudies/BAnQ_100TEMPS/100temps_Thumbnail-1280x960.jpg',
+    image2:'https://k72.ca/uploads/caseStudies/LAMAJEURE_-_Son_sur_mesure/chalaxeur-thumbnail_img-1280x960.jpg'
+  },
+  {
+    image1:'https://k72.ca/uploads/caseStudies/CRISIS24/crisis24_behance_1920X1200_cartes-1280x960.jpg',
+    image2:'https://k72.ca/uploads/caseStudies/Opto/thumbnailimage_opto-1280x960.jpg'
+  },
+  {
+    image1:'https://k72.ca/uploads/caseStudies/PME-MTL/PME-MTL_Thumbnail-1280x960.jpg',
+    image2:'https://k72.ca/uploads/caseStudies/FRUITE/Fruite_thumbnail_bbq-1280x960.jpg'
+  },
+  {
+    image1:'https://k72.ca/uploads/caseStudies/Opto_Reseau_Brand/opto_thumbnail2-1280x960.jpg',
+    image2:'https://k72.ca/uploads/caseStudies/OKA/OKA_thumbnail-1280x960.jpg'
+  },
+  {
+    image1:'https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960-1280x960.jpg',
+    image2:'https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---Thumbnail-1280x960.jpg'
+  }
+]
+
 
 const Projects = () => {
-  return (
-    <div>Projects</div>
-  )
-}
 
-export default Projects
+  gsap.registerPlugin(ScrollTrigger)
+
+  useGSAP(function(){
+    gsap.from('.hero',{
+      height:'100px',
+      stagger:{
+        amount:0.4
+      },
+      scrollTrigger:{
+        trigger:'.parent',
+        start:'top 50%',
+        end:'top -200%',
+        scrub:true
+      }
+    })
+  })
+
+  return (
+    <div>
+      <div className="pt-[30vh]">
+        <h2 className="font-[font2] text-[10vw] uppercase">Projects</h2>
+      </div>
+      <div className="-mt-15 parent">
+        {projectImages.map(function(elem,idx){
+          return <div key={idx} className="hero w-full h-[500px] mb-5 flex gap-5">
+          <ProjectCard image1={elem.image1} image2={elem.image2}/>
+          </div>
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
