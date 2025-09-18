54ce9c5521958651b9abc8e4fa33c842a6d4d48b
AnilKumt <anilkumt51@gmail.com>
2025-09-18 22:07:17 +0530

    made project page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,3 +1,77 @@
+// import React from "react";
+// import ProjectCard from "../components/projects/ProjectCard";
+// import { useGSAP } from "@gsap/react";
+// import gsap from "gsap";
+// import { ScrollTrigger } from "gsap/all";
+
+// const projectImages = [
+//   {
+//     image1:'https://k72.ca/uploads/caseStudies/COUP_FUMANT/CF_thumbnail-1280x960.jpg',
+//     image2:'https://k72.ca/uploads/caseStudies/SHELTON/thumbnailimage_shelton-1280x960.jpg'
+//   },
+//   {
+//     image1:'https://k72.ca/uploads/caseStudies/A_table/thumbnailimage_atable2-1280x960.jpg',
+//     image2:'https://k72.ca/uploads/caseStudies/SollioAg/thumbnailimage_SollioAg-1280x960.jpg'
+//   },
+//   {
+//     image1:'https://k72.ca/uploads/caseStudies/BAnQ_100TEMPS/100temps_Thumbnail-1280x960.jpg',
+//     image2:'https://k72.ca/uploads/caseStudies/LAMAJEURE_-_Son_sur_mesure/chalaxeur-thumbnail_img-1280x960.jpg'
+//   },
+//   {
+//     image1:'https://k72.ca/uploads/caseStudies/CRISIS24/crisis24_behance_1920X1200_cartes-1280x960.jpg',
+//     image2:'https://k72.ca/uploads/caseStudies/Opto/thumbnailimage_opto-1280x960.jpg'
+//   },
+//   {
+//     image1:'https://k72.ca/uploads/caseStudies/PME-MTL/PME-MTL_Thumbnail-1280x960.jpg',
+//     image2:'https://k72.ca/uploads/caseStudies/FRUITE/Fruite_thumbnail_bbq-1280x960.jpg'
+//   },
+//   {
+//     image1:'https://k72.ca/uploads/caseStudies/Opto_Reseau_Brand/opto_thumbnail2-1280x960.jpg',
+//     image2:'https://k72.ca/uploads/caseStudies/OKA/OKA_thumbnail-1280x960.jpg'
+//   },
+//   {
+//     image1:'https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960-1280x960.jpg',
+//     image2:'https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---Thumbnail-1280x960.jpg'
+//   }
+// ]
+
+// const Projects = () => {
+
+//   gsap.registerPlugin(ScrollTrigger)
+
+//   useGSAP(function(){
+//     gsap.from('.hero',{
+//       height:'100px',
+//       stagger:{
+//         amount:0.4
+//       },
+//       scrollTrigger:{
+//         trigger:'.parent',
+//         start:'top 50%',
+//         end:'top -200%',
+//         scrub:true
+//       }
+//     })
+//   })
+
+//   return (
+//     <div>
+//       <div className="sm:pt-[30vh] text-black">
+//         <h2 className="font-[font2] text-[10vw] uppercase">Projects</h2>
+//       </div>
+//       <div className="sm:-mt-15 parent md:-mt-8">
+//         {projectImages.map(function(elem,idx){
+//           return <div key={idx} className="hero w-full sm:h-[500px] mb-5 flex gap-5 h-[300px]">
+//           <ProjectCard image1={elem.image1} image2={elem.image2}/>
+//           </div>
+//         })}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Projects;
+
 import React from "react";
 import ProjectCard from "../components/projects/ProjectCard";
 import { useGSAP } from "@gsap/react";
@@ -6,68 +80,79 @@ import { ScrollTrigger } from "gsap/all";
 
 const projectImages = [
   {
-    image1:'https://k72.ca/uploads/caseStudies/COUP_FUMANT/CF_thumbnail-1280x960.jpg',
-    image2:'https://k72.ca/uploads/caseStudies/SHELTON/thumbnailimage_shelton-1280x960.jpg'
+    image1: "https://k72.ca/uploads/caseStudies/COUP_FUMANT/CF_thumbnail-1280x960.jpg",
+    image2: "https://k72.ca/uploads/caseStudies/SHELTON/thumbnailimage_shelton-1280x960.jpg",
   },
   {
-    image1:'https://k72.ca/uploads/caseStudies/A_table/thumbnailimage_atable2-1280x960.jpg',
-    image2:'https://k72.ca/uploads/caseStudies/SollioAg/thumbnailimage_SollioAg-1280x960.jpg'
+    image1: "https://k72.ca/uploads/caseStudies/A_table/thumbnailimage_atable2-1280x960.jpg",
+    image2: "https://k72.ca/uploads/caseStudies/SollioAg/thumbnailimage_SollioAg-1280x960.jpg",
   },
   {
-    image1:'https://k72.ca/uploads/caseStudies/BAnQ_100TEMPS/100temps_Thumbnail-1280x960.jpg',
-    image2:'https://k72.ca/uploads/caseStudies/LAMAJEURE_-_Son_sur_mesure/chalaxeur-thumbnail_img-1280x960.jpg'
+    image1: "https://k72.ca/uploads/caseStudies/BAnQ_100TEMPS/100temps_Thumbnail-1280x960.jpg",
+    image2: "https://k72.ca/uploads/caseStudies/LAMAJEURE_-_Son_sur_mesure/chalaxeur-thumbnail_img-1280x960.jpg",
   },
   {
-    image1:'https://k72.ca/uploads/caseStudies/CRISIS24/crisis24_behance_1920X1200_cartes-1280x960.jpg',
-    image2:'https://k72.ca/uploads/caseStudies/Opto/thumbnailimage_opto-1280x960.jpg'
+    image1: "https://k72.ca/uploads/caseStudies/CRISIS24/crisis24_behance_1920X1200_cartes-1280x960.jpg",
+    image2: "https://k72.ca/uploads/caseStudies/Opto/thumbnailimage_opto-1280x960.jpg",
   },
   {
-    image1:'https://k72.ca/uploads/caseStudies/PME-MTL/PME-MTL_Thumbnail-1280x960.jpg',
-    image2:'https://k72.ca/uploads/caseStudies/FRUITE/Fruite_thumbnail_bbq-1280x960.jpg'
+    image1: "https://k72.ca/uploads/caseStudies/PME-MTL/PME-MTL_Thumbnail-1280x960.jpg",
+    image2: "https://k72.ca/uploads/caseStudies/FRUITE/Fruite_thumbnail_bbq-1280x960.jpg",
   },
   {
-    image1:'https://k72.ca/uploads/caseStudies/Opto_Reseau_Brand/opto_thumbnail2-1280x960.jpg',
-    image2:'https://k72.ca/uploads/caseStudies/OKA/OKA_thumbnail-1280x960.jpg'
+    image1: "https://k72.ca/uploads/caseStudies/Opto_Reseau_Brand/opto_thumbnail2-1280x960.jpg",
+    image2: "https://k72.ca/uploads/caseStudies/OKA/OKA_thumbnail-1280x960.jpg",
   },
   {
-    image1:'https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960-1280x960.jpg',
-    image2:'https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---Thumbnail-1280x960.jpg'
-  }
-]
+    image1: "https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960-1280x960.jpg",
+    image2: "https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---Thumbnail-1280x960.jpg",
+  },
+];
 
 const Projects = () => {
+  gsap.registerPlugin(ScrollTrigger);
 
-  gsap.registerPlugin(ScrollTrigger)
-
-  useGSAP(function(){
-    gsap.from('.hero',{
-      height:'100px',
-      stagger:{
-        amount:0.4
+  useGSAP(() => {
+    gsap.from(".hero", {
+      height: "100px",
+      stagger: { amount: 0.4 },
+      scrollTrigger: {
+        trigger: ".parent",
+        start: "top 50%",
+        end: "top -200%",
+        scrub: true,
       },
-      scrollTrigger:{
-        trigger:'.parent',
-        start:'top 50%',
-        end:'top -200%',
-        scrub:true
-      }
-    })
-  })
+    });
+  });
 
   return (
-    <div>
-      <div className="pt-[30vh] text-black">
-        <h2 className="font-[font2] text-[10vw] uppercase">Projects</h2>
+    <div className="px-4 sm:px-8">
+      {/* Title */}
+      <div className="sm:pt-[30vh] pt-[15vh] text-black">
+        <h2 className="font-[font2] text-[12vw] sm:text-[10vw] uppercase leading-none">
+          Projects
+        </h2>
       </div>
-      <div className="-mt-15 parent md:-mt-8">
-        {projectImages.map(function(elem,idx){
-          return <div key={idx} className="hero w-full h-[500px] mb-5 flex gap-5">
-          <ProjectCard image1={elem.image1} image2={elem.image2}/>
+
+      {/* Projects List */}
+      <div className="parent mt-6 sm:-mt-15 md:-mt-8 flex flex-col gap-6">
+        {projectImages.map((elem, idx) => (
+          <div
+            key={idx}
+            className="
+              hero w-full 
+              h-[220px] xs:h-[250px] sm:h-[400px] lg:h-[500px]
+              flex flex-col sm:flex-row gap-3 sm:gap-5
+              rounded-lg overflow-hidden
+            "
+          >
+            <ProjectCard image1={elem.image1} image2={elem.image2} />
           </div>
-        })}
+        ))}
       </div>
     </div>
   );
 };
 
 export default Projects;
+
